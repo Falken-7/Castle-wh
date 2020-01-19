65172dd9bfd0d7d79ebc9e03269ab8ce9af8e580
Sarthak Sharma <54619077+Falken-7@users.noreply.github.com>
2020-01-19 21:16:50 +0530

    Update tower.js

--- tower.js
+++ tower.js
@@ -1,8 +1,8 @@
 class Tower1 {
     constructor(x,y,width,height){
         var options = {
-            'restitution':1,
-            'friction':0.8,
+            'restitution':0,
+            'friction':0,
             'density':1.0
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
